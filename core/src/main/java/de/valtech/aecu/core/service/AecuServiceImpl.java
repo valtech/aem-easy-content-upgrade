dbbf4ab3811b4c79cb5a3e28b9dc5f02971faeef
Roland Gruber <roland.gruber@valtech.de>
2018-05-07 16:12:16 +0200

    find script candidates

--- core/src/main/java/de/valtech/aecu/core/service/AecuServiceImpl.java
+++ core/src/main/java/de/valtech/aecu/core/service/AecuServiceImpl.java
@@ -16,10 +16,18 @@
  */
 package de.valtech.aecu.core.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.jackrabbit.JcrConstants;
 import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.jcr.resource.api.JcrResourceConstants;
+import org.apache.sling.settings.SlingSettingsService;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -37,7 +45,10 @@ import de.valtech.aecu.service.AecuService;
 public class AecuServiceImpl implements AecuService {
     
     @Reference
-    ServiceResourceResolverService resolverService;
+    private ServiceResourceResolverService resolverService;
+    
+    @Reference
+    private SlingSettingsService slingSettings;
 
     @Override
     public String getVersion() {
@@ -47,11 +58,91 @@ public class AecuServiceImpl implements AecuService {
     @Override
     public List<String> getFiles(String path) throws AecuException {
         try (ResourceResolver resolver = resolverService.getServiceResourceResolver()) {
-            return null;
+            return findCandidates(resolver, path);
         }
         catch (LoginException e) {
             throw new AecuException("Unable to get service resource resolver", e);
         }
+    }
+
+    /**
+     * Finds all candidates for scripts to run.
+     * 
+     * @param resolver service resource resolver
+     * @param path starting path
+     * @return candidate list
+     * @throws AecuException error finding candidates
+     */
+    private List<String> findCandidates(ResourceResolver resolver, String path) throws AecuException {
+        if (path == null) {
+            throw new AecuException("Path is null");
+        }
+        Resource resource = resolver.getResource(path);
+        if (resource == null) {
+            throw new AecuException("Path is invalid");
+        }
+        List<String> candidates = new ArrayList<>();
+        if (isFolder(resource) && matchesRunmodes(resource.getName())) {
+            Iterator<Resource> childIterator = resource.listChildren();
+            for (Resource child : resource.getChildren()) {
+                candidates.addAll(findCandidates(resolver, child.getPath()));
+            }
+        }
+        else if (isValidScriptName(resource.getName())) {
+            candidates.add(path);
+        }
+        return candidates;
+    }
+
+    /**
+     * Checks if the resource is a folder.
+     * 
+     * @param resource resource
+     * @return is folder
+     */
+    private boolean isFolder(Resource resource) {
+        String type = resource.getValueMap().get(JcrConstants.JCR_PRIMARYTYPE, String.class);
+        return JcrResourceConstants.NT_SLING_FOLDER.equals(type)
+                        || JcrResourceConstants.NT_SLING_ORDERED_FOLDER.equals(type)
+                        || JcrConstants.NT_FOLDER.equals(type);
+    }
+
+    /**
+     * Checks if the folder matches the system's run modes if specified in folder name.
+     * 
+     * @param name resource name
+     * @return matches run modes
+     */
+    protected boolean matchesRunmodes(String name) {
+        if (!name.contains(".")) {
+            return true;
+        }
+        Set<String> runModes = slingSettings.getRunModes();
+        String runModeString = name.substring(name.indexOf(".") + 1);
+        String[] combinations = runModeString.split(";");
+        for (String combination : combinations) {
+            String[] modes = combination.split("\\.");
+            if (runModes.containsAll(Arrays.asList(modes))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the name is a valid script.
+     * 
+     * @param name file name
+     * @return is valid
+     */
+    protected boolean isValidScriptName(String name) {
+        if (!name.endsWith(".groovy")) {
+            return false;
+        }
+        if (name.contains(".fallback.")) {
+            return false;
+        }
+        return true;
     }
 
 }
---
+++ core/src/test/java/de/valtech/aecu/core/service/AecuServiceImplTest.java
@@ -0,0 +1,103 @@
+/*
+ *  Copyright 2018 Valtech GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>
+ */
+package de.valtech.aecu.core.service;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.sling.settings.SlingSettingsService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ * Tests AecuServiceImpl
+ * 
+ * @author Roland Gruber
+ */
+@RunWith(value=MockitoJUnitRunner.class)
+public class AecuServiceImplTest {
+    
+    @InjectMocks
+    private AecuServiceImpl service;
+    
+    @Mock
+    private SlingSettingsService settingsService;
+    
+    @Before
+    public void setup() {
+        Set<String> runModes = new HashSet<>();
+        runModes.add("author");
+        runModes.add("test");
+        runModes.add("test2");
+        runModes.add("test3");
+        when(settingsService.getRunModes()).thenReturn(runModes);
+    }
+    
+    @Test
+    public void matchesRunmodes_noMode() {
+        assertTrue(service.matchesRunmodes("name"));
+    }
+
+    @Test
+    public void matchesRunmodes_nonMatching() {
+        assertFalse(service.matchesRunmodes("name.publish"));
+    }
+
+    @Test
+    public void matchesRunmodes_matching() {
+        assertTrue(service.matchesRunmodes("name.author"));
+        assertTrue(service.matchesRunmodes("name.test"));
+    }
+
+    @Test
+    public void matchesRunmodes_matchingMulti() {
+        assertTrue(service.matchesRunmodes("name.author.test"));
+        assertTrue(service.matchesRunmodes("name.author.test;testNO"));
+        assertTrue(service.matchesRunmodes("name.authorNO.test;test2.author"));
+    }
+
+    @Test
+    public void matchesRunmodes_nonMatchingMulti() {
+        assertFalse(service.matchesRunmodes("name.author.testNO"));
+        assertFalse(service.matchesRunmodes("name.author.testNO;testNO"));
+        assertFalse(service.matchesRunmodes("name.author.testNO;test2NO.author"));
+    }
+
+    @Test
+    public void isValidScriptName_ok() {
+        assertTrue(service.isValidScriptName("test.groovy"));
+    }
+
+    @Test
+    public void isValidScriptName_invalidExtension() {
+        assertFalse(service.isValidScriptName("test.txt"));
+    }
+
+    @Test
+    public void isValidScriptName_fallback() {
+        assertFalse(service.isValidScriptName("test.fallback.groovy"));
+    }
+
+}
