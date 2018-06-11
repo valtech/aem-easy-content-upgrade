8d26763857b4aae9df9f911d77ba9fdbfd07f36e
Bryan Gerhard Chavez <bryan.chavez@valtech.de>
2018-06-11 16:04:47 +0200

    Execute Servlet for executing groovy scripts.

---
+++ core/src/main/java/de/valtech/aecu/core/service/ExecutionServlet.java
@@ -0,0 +1,42 @@
+/*
+ *  Copyright 2018 Valtech GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>
+ */
+package de.valtech.aecu.core.service;
+
+
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+
+@SlingServlet(label = "Valtech AECU :: Execute", methods = { "GET" }, paths = { "/bin/public/valtech/aecu/execute"}, metatype = false)
+public class ExecutionServlet extends SlingAllMethodsServlet {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExecutionServlet.class);
+
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("hola");
+    }
+
+}
