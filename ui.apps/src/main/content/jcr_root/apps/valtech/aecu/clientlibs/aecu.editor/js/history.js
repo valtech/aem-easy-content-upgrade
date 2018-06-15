3c1d31b8fb81f75d94545987eadb2a8d5f425a87
Roland Gruber <roland.gruber@valtech.de>
2018-06-15 11:02:52 +0200

    added pre tags + refactoring

--- ui.apps/src/main/content/jcr_root/apps/valtech/aecu/clientlibs/aecu.editor/js/history.js
+++ ui.apps/src/main/content/jcr_root/apps/valtech/aecu/clientlibs/aecu.editor/js/history.js
@@ -1,18 +1,28 @@
 $(document).ready(function () {
 	
-	var search = window.location.search;
-	if (search && search.includes('aecuScriptPath')) {
-		search = search.substring(1);
-		params = search.split('&');
-		for (var i = 0; i < params.length; i++) {
-			var param = params[i];
-			var parts = param.split('=');
-			if (parts[0] == 'aecuScriptPath') {
-				var name = parts[1];
-				var item = jQuery("coral-accordion-item[data-path='" + name + "']");
-				item[0].selected = true;
+	AECU.History = {};
+	
+	/**
+	 * Select the accordion item based on the GET parameter.
+	 */
+	AECU.History.selectAccordion = function() {
+		var search = window.location.search;
+		if (search && search.includes('aecuScriptPath')) {
+			search = search.substring(1);
+			params = search.split('&');
+			for (var i = 0; i < params.length; i++) {
+				var param = params[i];
+				var parts = param.split('=');
+				if (parts[0] == 'aecuScriptPath') {
+					var name = parts[1];
+					var item = jQuery("coral-accordion-item[data-path='" + name + "']");
+					item[0].selected = true;
+				}
 			}
 		}
-	}
+	};
+	
+	
+	AECU.History.selectAccordion();
 	
 });
