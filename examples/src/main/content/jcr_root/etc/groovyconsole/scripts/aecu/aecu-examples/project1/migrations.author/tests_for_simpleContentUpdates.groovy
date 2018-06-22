f2e2cfce0e5eb9014eca3b320b351dfb3aa3d444
Roxana Muresan <roxana.muresan@valtech.de>
2018-06-22 18:00:43 +0200

    AECU-Binding-sample: updated groovy test script.

--- examples/src/main/content/jcr_root/etc/groovyconsole/scripts/aecu/aecu-examples/project1/migrations.author/tests_for_simpleContentUpdates.groovy
+++ examples/src/main/content/jcr_root/etc/groovyconsole/scripts/aecu/aecu-examples/project1/migrations.author/tests_for_simpleContentUpdates.groovy
@@ -1,24 +1,47 @@
-println "simpleContentUpdate is $aecu"
+def conditionMap_Hero1 = [:]
+conditionMap_Hero1['sling:resourceType'] = "weretail/components/content/heroimage"
+def conditionMap_Hero2 = [:]
+conditionMap_Hero2['fileReference'] = "/content/dam/we-retail/en/activities/running/fitness-woman.jpg"
 
-def conditionMapHero = [:]
-conditionMapHero['sling:resourceType'] = "weretail/components/content/heroimage"
-conditionMapHero['fileReference'] = "/content/dam/we-retail/en/activities/running/fitness-woman.jpg"
-
-def conditionMapPage = [:]
-conditionMapPage['jcr:primaryType'] = "cq:PageContent"
+def conditionMap_Page = [:]
+conditionMap_Page['jcr:primaryType'] = "cq:PageContent"
 
 
-aecu
-// test filter methods
-        .forResources((String[])["/content/we-retail/ca/en/jcr:content","/invalid/path", "/content/we-retail/ca/en/experience/jcr:content"])
-        .forChildResourcesOf("/content/we-retail/ca/en/women")
-        .forDescendantResourcesOf("/content/we-retail/ca/en/men")
-        .forChildResourcesOfWithProperties("/content/we-retail/ca/en/products", conditionMapPage)
-        .forDescendantResourcesOfWithProperties("/content/we-retail/ca/en/equipment", conditionMapHero)
-        .forDescendantResourcesOfWithProperties("/content/we-retail/ca/en/women", conditionMapHero)
-
-// test editor methods
-        .doSetProperty("newProperty", "added by aecu")
+def return1 = aecu.getNewMigration()
+// traversers
+        .forResources((String[])["/content/we-retail/ca/en/jcr:content", "/content/we-retail/ca/en/experience/jcr:content"]) //,"/invalid/path"
+        .forChildResourcesOf("/content/we-retail/ca/en/men")
+        .forDescendantResourcesOf("/content/we-retail/ca/en/women")
+// filters
+        .filterByProperties(conditionMap_Page)
+// actions
+        .doSetProperty("newProperty", "aecu test with conditionMap_Page")
         .doRenameProperty("newProperty", "delete_me_later")
 //    .removeProperty("delete_me_later")
         .apply()
+println "$return1"
+
+
+def complexFilter = new ORFilter(
+        [ new FilterByProperties(conditionMap_Page),
+          new ANDFilter( [
+                  new FilterByProperties(conditionMap_Hero1),
+                  new FilterByProperties(conditionMap_Hero2)
+          ] )
+        ])
+
+def return2 = aecu.getNewMigration()
+// traversers
+        .forResources((String[])["/content/we-retail/ca/en/jcr:content", "/content/we-retail/ca/en/experience/jcr:content"]) //,"/invalid/path"
+        .forChildResourcesOf("/content/we-retail/ca/en/men")
+        .forDescendantResourcesOf("/content/we-retail/ca/en/women")
+// filters
+        .filterWith(complexFilter)
+// actions
+        .doSetProperty("newProperty", "aecu test with conditionMap_Hero")
+        .doRenameProperty("newProperty", "delete_me_later")
+//    .removeProperty("delete_me_later")
+        .apply()
+println "$return2"
+
+return return1 + return2
