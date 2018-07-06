2ba82a8b20dd122dea49319a24b4400c3f65b944
Roxana Muresan <roxana.muresan@valtech.de>
2018-07-06 18:23:28 +0200

    bindings/improvements-1: added javascript for inserting aecu bindings and imports

---
+++ ui.apps/src/main/content/jcr_root/apps/valtech/aecu/clientlibs/aecu/groovyconsole/js/aecu_groovyconsole_extension.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    if ($("#bindings").length != 0) {
+        $("#bindings ul").append("<li>aecu - de.valtech.aecu.core.groovy.console.bindings.SimpleContentUpdate</li>"); // TODO: url to some javadoc available public
+    }
+
+    if ($("#imports").length != 0) {
+        $("#imports ul").append("<li>de.valtech.aecu.core.groovy.console.bindings.filters</li>"); // TODO: url to some javadoc available public
+    }
+
+});
