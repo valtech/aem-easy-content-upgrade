fb822ad902e37c25c948b42e5998f36434660db7
Roxana Muresan <roxana.muresan@valtech.de>
2018-07-12 11:52:20 +0200

    bindings/improvements-1: updated sample groovy script

--- examples/src/main/content/jcr_root/etc/groovyconsole/scripts/aecu/aecu-examples/project1/migrations.author/tests_for_simpleContentUpdates.groovy
+++ examples/src/main/content/jcr_root/etc/groovyconsole/scripts/aecu/aecu-examples/project1/migrations.author/tests_for_simpleContentUpdates.groovy
@@ -9,41 +9,41 @@ conditionMap_Page['jcr:primaryType'] = "cq:PageContent"
 
 println aecu.contentUpgradeBuilder()
 // traversers
-        .forResources((String[]) ["/content/we-retail/ca/en/jcr:content", "/content/we-retail/ca/en/experience/jcr:content"]) //,"/invalid/path"
+        .forResources((String[])["/content/we-retail/ca/en/jcr:content", "/content/we-retail/ca/en/experience/jcr:content"]) //,"/invalid/path"
         .forChildResourcesOf("/content/we-retail/ca/en/men")
         .forDescendantResourcesOf("/content/we-retail/ca/en/women")
 // filters
         .filterByProperties(conditionMap_Page)
 // actions
-        .doSetStringProperty("newStringProperty", "aecu test with conditionMap_Page")
-        .doSetBooleanProperty("newBooleanProperty", true)
-        .doSetIntegerProperty("newStringProperty", 123)
-        .doRenameProperty("newStringProperty", "delete_me_later")
+        .doSetStringProperty("newStringProperty1", "aecu test with conditionMap_Page")
+        .doSetBooleanProperty("newBooleanProperty1", true)
+        .doSetIntegerProperty("newIntegerProperty1", 123)
+        .doRenameProperty("newStringProperty1", "delete_me_later")
 //    .removeProperty("delete_me_later")
-        .run()
+        .dryRun()
 
 
 
 
-def complexFilter = new ORFilter(
-        [new FilterByProperties(conditionMap_Page),
-         new ANDFilter([
-                 new FilterByProperties(conditionMap_Hero1),
-                 new FilterByProperties(conditionMap_Hero2)
-         ])
+def complexFilter =  new ORFilter(
+        [ new FilterByProperties(conditionMap_Page),
+          new ANDFilter( [
+                  new FilterByProperties(conditionMap_Hero1),
+                  new FilterByProperties(conditionMap_Hero2)
+          ] )
         ])
 
 println aecu.contentUpgradeBuilder()
 // traversers
-        .forResources((String[]) ["/content/we-retail/ca/en/jcr:content", "/content/we-retail/ca/en/experience/jcr:content"]) //,"/invalid/path"
+        .forResources((String[])["/content/we-retail/ca/en/jcr:content", "/content/we-retail/ca/en/experience/jcr:content"]) //,"/invalid/path"
         .forChildResourcesOf("/content/we-retail/ca/en/men")
         .forDescendantResourcesOf("/content/we-retail/ca/en/women")
 // filters
         .filterWith(complexFilter)
 // actions
-        .doSetProperty("newStringProperty", "aecu test with conditionMap_Hero")
-        .doSetBooleanProperty("newBooleanProperty", false)
-        .doSetIntegerProperty("newStringProperty", 789)
-        .doRenameProperty("newStringProperty", "delete_me_later")
+        .doSetStringProperty("newStringProperty2", "aecu test with conditionMap_Hero")
+        .doSetBooleanProperty("newBooleanProperty2", false)
+        .doSetIntegerProperty("newIntegerProperty2", 789)
+        .doRenameProperty("newStringProperty2", "delete_me_later2")
 //    .removeProperty("delete_me_later")
-        .run()
+        .dryRun()
