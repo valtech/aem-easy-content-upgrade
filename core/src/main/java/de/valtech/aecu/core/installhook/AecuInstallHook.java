2c26efe8e991ee926d27e41a9f4852198954a5f9
Christopher Piosecny <christopher.piosecny@valtech.de>
2018-07-16 11:44:04 +0200

    Adapt InstallHook, only process groovy scripts matching runmode that have never been executed or have been modified.

--- core/src/main/java/de/valtech/aecu/core/installhook/AecuInstallHook.java
+++ core/src/main/java/de/valtech/aecu/core/installhook/AecuInstallHook.java
@@ -23,93 +23,136 @@ import de.valtech.aecu.service.AecuService;
 import de.valtech.aecu.service.ExecutionResult;
 import de.valtech.aecu.service.HistoryEntry;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.jackrabbit.vault.fs.api.ProgressTrackerListener;
+import org.apache.jackrabbit.vault.fs.io.Archive;
 import org.apache.jackrabbit.vault.packaging.InstallContext;
 import org.apache.jackrabbit.vault.packaging.InstallHook;
 import org.apache.jackrabbit.vault.packaging.PackageException;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * InstallHook handling installation of groovy scripts.
+ * InstallHook handling installation of groovy scripts. The InstallHook gathers groovy scripts contained in the installed vault
+ * package and executes them depending on active runmodes and if the script has been added, modified or never executed. <br>
+ * Example usage in content-package-maven-plugin:
+ * <pre>
+ * {@code
+ * <plugin>
+ *     <groupId>com.day.jcr.vault</groupId>
+ *     <artifactId>content-package-maven-plugin</artifactId>
+ *     <extensions>true</extensions>
+ *     <configuration>
+ *         <filterSource>src/main/content/META-INF/vault/filter.xml</filterSource>
+ *         <verbose>true</verbose>
+ *         <failOnError>true</failOnError>
+ *         <group>Valtech</group>
+ *         <properties>
+ *             <installhook.aecu.class>de.valtech.aecu.core.installhook.AecuInstallHook</installhook.aecu.class>
+ *         </properties>
+ *     </configuration>
+ * </plugin>
+ * }
+ * </pre>
  */
 public class AecuInstallHook implements InstallHook {
 
     private static final Logger LOG = LoggerFactory.getLogger(AecuInstallHook.class);
 
-    private final BundleContext bundleContext;
+    private final OsgiServiceProvider osgiServiceProvider;
     private AecuTrackerListener listener;
 
     public AecuInstallHook() {
-        Bundle currentBundle = FrameworkUtil.getBundle(this.getClass());
-        if (currentBundle == null) {
-            throw new IllegalStateException("The class " + this.getClass() + " was not loaded through a bundle classloader");
-        }
-        bundleContext = currentBundle.getBundleContext();
-        if (bundleContext == null) {
-            throw new IllegalStateException("Could not get bundle context for bundle " + currentBundle);
-        }
-
-    }
-
-    private ServiceReference<AecuService> getAecuServiceReference() {
-        ServiceReference<AecuService> aecuServiceReference = bundleContext.getServiceReference(AecuService.class);
-        if (aecuServiceReference == null) {
-            throw new IllegalStateException("Could not retrieve service reference for AECUService.");
-        }
-        return aecuServiceReference;
+        LOG.info("Instantiating install hook");
+        this.osgiServiceProvider = new OsgiServiceProvider(this.getClass());
     }
 
     @Override
     public void execute(InstallContext installContext) throws PackageException {
         LOG.info("Executing in phase {}", installContext.getPhase());
-        ServiceReference<AecuService> aecuServiceReference = getAecuServiceReference();
-        AecuService aecuService = bundleContext.getService(aecuServiceReference);
-        if (aecuService == null) {
-            throw new IllegalStateException("Could not get the AECU service, verify that the bundle was installed correctly!");
-        }
+        ServiceReference<AecuService> aecuServiceReference = osgiServiceProvider.getServiceReference(AecuService.class);
+        AecuService aecuService = osgiServiceProvider.getService(aecuServiceReference);
+
         try {
             switch (installContext.getPhase()) {
                 case PREPARE:
                     ProgressTrackerListener originalListener = installContext.getOptions().getListener();
-                    listener = new AecuTrackerListener(originalListener, aecuService, "A", "M", "-");
+                    listener = new AecuTrackerListener(originalListener, aecuService);
                     installContext.getOptions().setListener(listener);
                     break;
                 case INSTALLED:
-                    boolean isDryRun = installContext.getOptions().isDryRun();
-                    if (isDryRun) {
-                        LOG.debug("Skipping any modifications, dry run mode selected");
-                    } else {
-                        HistoryEntry installationHistory = aecuService.createHistoryEntry();
-                        for (String groovyScriptPath : listener.getGroovyScriptPaths()) {
-                            LOG.info("Executing script {}", groovyScriptPath);
-                            ExecutionResult result = aecuService.execute(groovyScriptPath);
-                            installationHistory = aecuService.storeExecutionInHistory(installationHistory, result);
-                            LOG.info("Executed script {}: {}", groovyScriptPath, result.getOutput());
-                        }
-                        aecuService.finishHistoryEntry(installationHistory);
-                        // TODO: from my point of view the installation should not be failed in case
-                        // a fallback script was executed successfully
-                        if (!installationHistory.getResult().equals(HistoryEntry.RESULT.SUCCESS)) {
-                            throw new PackageException("Failed installation, check installation history at "
-                                    + installationHistory.getRepositoryPath());
-                        }
+                    HistoryEntry installationHistory = executeScripts(aecuService, installContext);
+                    // TODO: decide if we throw an exception or not.
+                    // For example, when a fallback script can be executed but the original script failed the history would
+                    // be marked as failed.
+                    if (!installationHistory.getSingleResults().isEmpty() &&
+                            !HistoryEntry.RESULT.SUCCESS.equals(installationHistory.getResult())) {
+                        throw new PackageException(
+                                "Failed installation, check installation history at " + installationHistory.getRepositoryPath());
                     }
                     break;
                 default:
                     break;
             }
-        } catch (AecuException e) {
+        } catch (IOException | AecuException e) {
             throw new PackageException(e);
         } finally {
-            if (aecuServiceReference != null) {
-                bundleContext.ungetService(aecuServiceReference);
+            osgiServiceProvider.ungetService(aecuServiceReference);
+        }
+    }
+
+    private HistoryEntry executeScripts(AecuService aecuService, InstallContext installContext)
+            throws AecuException, IOException {
+        HistoryEntry installationHistory = aecuService.createHistoryEntry();
+        Archive archive = installContext.getPackage().getArchive();
+        List<String> allValidScriptCandidatesInArchive = findCandidates("", archive.getJcrRoot(), aecuService);
+        List<String> modifiedOrAddedScriptPaths = listener.getModifiedOrAddedPaths();
+        for (String groovyScriptPath : allValidScriptCandidatesInArchive) {
+            HookExecutionHistory hookExecutionHistory = new HookExecutionHistory(installContext.getSession(), groovyScriptPath);
+            if (modifiedOrAddedScriptPaths.contains(groovyScriptPath) || !hookExecutionHistory.hasBeenExecutedBefore()) {
+                try {
+                    installationHistory = executeScript(aecuService, installationHistory, groovyScriptPath);
+                    hookExecutionHistory.setExecuted();
+                } catch (AecuException e) {
+                    LOG.warn("Error executing script " + groovyScriptPath, e);
+                }
             }
         }
+        installationHistory = aecuService.finishHistoryEntry(installationHistory);
+        return installationHistory;
+    }
+
+    private HistoryEntry executeScript(AecuService aecuService, HistoryEntry installationHistory, String groovyScriptPath)
+            throws AecuException {
+        LOG.info("Executing script {}", groovyScriptPath);
+        ExecutionResult result = aecuService.execute(groovyScriptPath);
+        installationHistory = aecuService.storeExecutionInHistory(installationHistory, result);
+        LOG.info("Executed script {}: {}", groovyScriptPath, result.getOutput());
+        return installationHistory;
+    }
+
+    // mildly duplicated
+    private List<String> findCandidates(String parent, Archive.Entry entry, AecuService aecuService) {
+        List<String> candidates = new ArrayList<>();
+        if (entry == null) {
+            return candidates;
+        }
+        String entryName = entry.getName();
+        String entryPath = parent + "/" + entryName;
+
+        if (entry.isDirectory() && aecuService.matchesRunmodes(entryName)) {
+            for (Archive.Entry childEntry : entry.getChildren()) {
+                candidates.addAll(findCandidates(entryPath, childEntry, aecuService));
+            }
+        } else if (aecuService.isValidScriptName(entryName)) {
+            candidates.add(StringUtils.substringAfter(entryPath, "/jcr_root"));
+        }
+        return candidates;
     }
 
 }
--- core/src/main/java/de/valtech/aecu/core/installhook/AecuTrackerListener.java
+++ core/src/main/java/de/valtech/aecu/core/installhook/AecuTrackerListener.java
@@ -18,10 +18,8 @@
  */
 package de.valtech.aecu.core.installhook;
 
-import de.valtech.aecu.service.AecuException;
 import de.valtech.aecu.service.AecuService;
 
-import org.apache.jackrabbit.util.Text;
 import org.apache.jackrabbit.vault.fs.api.ProgressTrackerListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,52 +27,57 @@ import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
+import javax.annotation.Nonnull;
+
 /**
- * Collects groovy script paths to execute.
+ * Collects groovy script paths to potentially execute based on the given actions.
  */
 public class AecuTrackerListener implements ProgressTrackerListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(AecuTrackerListener.class);
 
-    private static final String SCRIPT_PARENT_PATH = "/etc/groovyconsole/scripts/aecu/";
+    private static final Set<String> ACTIONS = new HashSet<>(Arrays.asList("A", "M"));
 
     private final ProgressTrackerListener originalListener;
     private final AecuService aecuService;
-    private final Set<String> actions;
-    private final Set<String> paths;
+    private final List<String> paths;
 
-    public AecuTrackerListener(ProgressTrackerListener originalListener, AecuService aecuService, String... actions) {
+    /**
+     * Constructor.
+     * @param originalListener the original ProgressTrackerListener.
+     * @param aecuService an AecuService instance.
+     */
+    public AecuTrackerListener(ProgressTrackerListener originalListener, AecuService aecuService) {
         this.originalListener = originalListener;
         this.aecuService = aecuService;
-        this.actions = new HashSet<>(Arrays.asList(actions));
-        this.paths = new HashSet<>();
+        this.paths = new LinkedList<>();
     }
 
-    public Set<String> getGroovyScriptPaths() {
-        return Collections.unmodifiableSet(paths);
+    /**
+     * Returns an unmodifiable list of the modified or added paths encountered during the installation phase.
+     * @return a list of modified or added paths, can be empty.
+     */
+    @Nonnull
+    public List<String> getModifiedOrAddedPaths() {
+        return Collections.unmodifiableList(paths);
     }
 
     @Override
     public void onMessage(Mode mode, String action, String path) {
         originalListener.onMessage(mode, action, path);
 
-        if (!actions.contains(action)) {
+        if (!ACTIONS.contains(action)) {
             LOG.debug("Skipping {} due to non matching action {}", path, action);
             return;
         }
-        // TODO: not the best idea to couple the detection of project roots to the structure
-        if (path.startsWith(SCRIPT_PARENT_PATH)) {
-            String pathToUse = Text.getAbsoluteParent(path, 4);
-            // grouping items by first sub folder below SCRIPT_PARENT_PATH
-            LOG.info("Found matching path {}, using parent {}", path, pathToUse);
-            try {
-                paths.addAll(aecuService.getFiles(pathToUse));
-            } catch (AecuException e) {
-                // TODO re-throw?
-                LOG.error(e.getMessage(), e);
-            }
+
+        if (aecuService.isValidScriptName(path)) {
+            LOG.debug("Found valid script path {}", path);
+            paths.add(path);
         }
     }
 
