9cad6f43bec6e1597b0e01d89da629ff83f0a8c6
Roxana Muresan <roxana.muresan@valtech.de>
2018-08-09 12:16:58 +0200

    groovy-scripts-examples: used a simpler example for script 3

--- examples/src/main/content/jcr_root/etc/groovyconsole/scripts/aecu/aecu-examples/project1/migrations.author/script3.groovy
+++ examples/src/main/content/jcr_root/etc/groovyconsole/scripts/aecu/aecu-examples/project1/migrations.author/script3.groovy
@@ -1,33 +1,16 @@
-/* Demo #1:
-   We want to replace responsivegrid pars with a separate resourceType while keeping the responsive grid as it is
-   We assume the /apps/<prj> part will be deployed separately with the release of the project
-*/
-
-/* Step 1: replace references in /content/<prj> */
+/**
+ * Updating the value of a property for all components ound under a certain path
+ */
 def conditionMap_resourceType = [:]
-conditionMap_resourceType['sling:resourceType'] = "wcm/foundation/components/responsivegrid"
+conditionMap_resourceType['sling:resourceType'] = "weretail/components/content/heroimage"
 
 println aecu.contentUpgradeBuilder()
 // traversers
-        .forDescendantResourcesOf("/content/we-retail")
+        .forDescendantResourcesOf("/content/we-retail/ca/en")
 // filters
         .filterByProperties(conditionMap_resourceType)
-        .filterByNodeNameRegex("par.*")
 // actions
         .printPath()
-        .doSetProperty("sling:resourceType", "wcm/foundation/components/custom_par")
+        .doSetProperty("useFullWidth", false)
         .dryRun()
-
-/* Step 2: add new component references in /etc/designs/<prj> without overwriting the responsivegrid references */
-println aecu.contentUpgradeBuilder()
-// traversers
-        .forDescendantResourcesOf("/etc/designs/we-retail")
-        .forDescendantResourcesOf("/etc/designs/we-retail-client-app")
-        .forDescendantResourcesOf("/etc/designs/we-retail-screens")
-        .forDescendantResourcesOf("/etc/designs/we-unlimited-app")
-// filters
-        .filterByMultiValuePropContains("components", (String[])["/libs/wcm/foundation/components/responsivegrid"])
-// actions
-        .printPath()
-        .doAddValuesToMultiValueProperty("components", (String[])["/libs/wcm/foundation/components/responsivegrid_par"])
-        .dryRun()
+        //.run()
