08049d5a1d9e4e5607761c2ab1dbe1f7837c9c4d
Roland Gruber <roland.gruber@valtech.de>
2018-10-18 11:16:31 +0200

    #39 unit tests

--- core/src/test/java/de/valtech/aecu/core/service/AecuServiceImplTest.java
+++ core/src/test/java/de/valtech/aecu/core/service/AecuServiceImplTest.java
@@ -24,14 +24,21 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.apache.jackrabbit.JcrConstants;
+import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.jcr.resource.api.JcrResourceConstants;
 import org.apache.sling.settings.SlingSettingsService;
 import org.junit.Before;
 import org.junit.Test;
@@ -41,6 +48,12 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import de.valtech.aecu.api.service.AecuException;
+import de.valtech.aecu.api.service.HistoryEntry;
+import de.valtech.aecu.api.service.HistoryEntry.STATE;
+import de.valtech.aecu.core.history.HistoryUtil;
+import de.valtech.aecu.core.serviceuser.ServiceResourceResolverService;
+
 /**
  * Tests AecuServiceImpl
  *
@@ -48,6 +61,10 @@ import org.mockito.junit.MockitoJUnitRunner;
  */
 @RunWith(value = MockitoJUnitRunner.class)
 public class AecuServiceImplTest {
+
+    private static final String DIR = "/dir";
+
+    private static final String FILE1 = "file1.groovy";
 
     @InjectMocks
     @Spy
@@ -57,16 +74,23 @@ public class AecuServiceImplTest {
     private SlingSettingsService settingsService;
 
     @Mock
+    private ServiceResourceResolverService resolverService;
+
+    @Mock
     private ResourceResolver resolver;
 
+    @Mock
+    private HistoryUtil historyUtil;
+
     @Before
-    public void setup() {
+    public void setup() throws LoginException {
         Set<String> runModes = new HashSet<>();
         runModes.add("author");
         runModes.add("test");
         runModes.add("test2");
         runModes.add("test3");
         when(settingsService.getRunModes()).thenReturn(runModes);
+        when(resolverService.getServiceResourceResolver()).thenReturn(resolver);
     }
 
     @Test
@@ -133,6 +157,99 @@ public class AecuServiceImplTest {
         verify(resolver, never()).getResource("/path/to/script.fallback.groovy");
 
         assertNull(service.getFallbackScript(resolver, "/path/to/script.fallback.groovy"));
+    }
+
+    @Test(expected = AecuException.class)
+    public void getFiles_invalidPath() throws AecuException {
+        service.getFiles(FILE1);
+    }
+
+    @Test
+    public void getFiles_validSinglePath() throws AecuException {
+        Resource fileResource = mock(Resource.class);
+        when(resolver.getResource(FILE1)).thenReturn(fileResource);
+        when(fileResource.getName()).thenReturn(FILE1);
+        ValueMap fileValues = mock(ValueMap.class);
+        when(fileValues.get(JcrConstants.JCR_PRIMARYTYPE, String.class)).thenReturn(JcrConstants.NT_FILE);
+        when(fileResource.getValueMap()).thenReturn(fileValues);
+
+        assertEquals(Arrays.asList(FILE1), service.getFiles(FILE1));
+    }
+
+    @Test
+    public void getFiles_validDirectoryPath() throws AecuException {
+        Resource dirResource = mock(Resource.class);
+        when(resolver.getResource(DIR)).thenReturn(dirResource);
+        when(dirResource.getName()).thenReturn("dir.author");
+        ValueMap dirValues = mock(ValueMap.class);
+        when(dirValues.get(JcrConstants.JCR_PRIMARYTYPE, String.class)).thenReturn(JcrResourceConstants.NT_SLING_FOLDER);
+        when(dirResource.getValueMap()).thenReturn(dirValues);
+
+        Resource fileResource = mock(Resource.class);
+        when(resolver.getResource(DIR + "/" + FILE1)).thenReturn(fileResource);
+        when(fileResource.getName()).thenReturn(FILE1);
+        ValueMap fileValues = mock(ValueMap.class);
+        when(fileValues.get(JcrConstants.JCR_PRIMARYTYPE, String.class)).thenReturn(JcrConstants.NT_FILE);
+        when(fileResource.getValueMap()).thenReturn(fileValues);
+        when(fileResource.getPath()).thenReturn(DIR + "/" + FILE1);
+
+        when(dirResource.getChildren()).thenReturn(Arrays.asList(fileResource));
+        when(dirResource.getChild(FILE1)).thenReturn(fileResource);
+
+        assertEquals(Arrays.asList(DIR + "/" + FILE1), service.getFiles(DIR));
+    }
+
+    @Test
+    public void getFiles_wrongRunmode() throws AecuException {
+        Resource dirResource = mock(Resource.class);
+        when(resolver.getResource(DIR)).thenReturn(dirResource);
+        when(dirResource.getName()).thenReturn("dir.invalid");
+        ValueMap dirValues = mock(ValueMap.class);
+        when(dirValues.get(JcrConstants.JCR_PRIMARYTYPE, String.class)).thenReturn(JcrResourceConstants.NT_SLING_FOLDER);
+        when(dirResource.getValueMap()).thenReturn(dirValues);
+
+        assertEquals(Arrays.asList(), service.getFiles(DIR));
+    }
+
+    @Test
+    public void getHistory() throws AecuException {
+        when(historyUtil.getHistory(0, 1, resolver)).thenReturn(Arrays.asList(mock(HistoryEntry.class)));
+
+        List<HistoryEntry> entries = service.getHistory(0, 1);
+
+        assertEquals(1, entries.size());
+    }
+
+    @Test(expected = AecuException.class)
+    public void storeExecutionInHistory_invalid() throws AecuException {
+        service.storeExecutionInHistory(null, null);
+    }
+
+    @Test
+    public void storeExecutionInHistory() throws AecuException {
+        HistoryEntryImpl history = new HistoryEntryImpl();
+        history.setState(STATE.RUNNING);
+
+        service.storeExecutionInHistory(history, null);
+
+        verify(historyUtil, times(1)).storeExecutionInHistory(history, null, resolver);
+    }
+
+    @Test
+    public void finishHistoryEntry() throws AecuException {
+        HistoryEntryImpl history = new HistoryEntryImpl();
+        history.setState(STATE.RUNNING);
+
+        service.finishHistoryEntry(history);
+
+        verify(historyUtil, times(1)).finishHistoryEntry(history, resolver);
+    }
+
+    @Test
+    public void createHistoryEntry() throws AecuException {
+        service.createHistoryEntry();
+
+        verify(historyUtil, times(1)).createHistoryEntry(resolver);
     }
 
 }
