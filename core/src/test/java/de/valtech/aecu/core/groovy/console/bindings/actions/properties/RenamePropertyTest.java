74f59a7b0d8d46e25295416b82eb0e6ab59a5e6d
Roland Gruber <roland.gruber@valtech.de>
2018-10-18 10:21:43 +0200

    #39 unit tests

---
+++ core/src/test/java/de/valtech/aecu/core/groovy/console/bindings/actions/properties/RenamePropertyTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018 Valtech GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package de.valtech.aecu.core.groovy.console.bindings.actions.properties;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.apache.sling.api.resource.ModifiableValueMap;
+import org.apache.sling.api.resource.PersistenceException;
+import org.apache.sling.api.resource.Resource;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ * Tests RenameProperty
+ * 
+ * @author Roland Gruber
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class RenamePropertyTest {
+
+    private static final String VAL1 = "val1";
+
+    private static final String ATTR_OLD = "attrOld";
+    private static final String ATTR_NEW = "attrNew";
+
+    @Mock
+    private Resource resource;
+
+    @Mock
+    private ModifiableValueMap valueMap;
+
+    @Before
+    public void setup() {
+        when(resource.adaptTo(ModifiableValueMap.class)).thenReturn(valueMap);
+    }
+
+    @Test
+    public void doAction() throws PersistenceException {
+        when(valueMap.remove(ATTR_OLD)).thenReturn(VAL1);
+        RenameProperty action = new RenameProperty(ATTR_OLD, ATTR_NEW);
+
+        action.doAction(resource);
+
+        verify(valueMap, times(1)).remove(ATTR_OLD);
+        verify(valueMap, times(1)).put(ATTR_NEW, VAL1);
+    }
+
+}
