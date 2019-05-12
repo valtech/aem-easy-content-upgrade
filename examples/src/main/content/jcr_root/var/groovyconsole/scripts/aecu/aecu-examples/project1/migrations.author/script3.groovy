8ed1a8f178133f9e953dbf5a5ac3dc107377fdb4
Roxana Muresan <roxana.muresan@valtech.de>
2019-05-12 21:37:11 +0200

    feature/2.0_remove_println: removed println from examples

--- examples/src/main/content/jcr_root/var/groovyconsole/scripts/aecu/aecu-examples/project1/migrations.author/script3.groovy
+++ examples/src/main/content/jcr_root/var/groovyconsole/scripts/aecu/aecu-examples/project1/migrations.author/script3.groovy
@@ -2,13 +2,12 @@
 def conditionMap_resourceType = [:]
 conditionMap_resourceType['sling:resourceType'] = "weretail/components/content/heroimage"
 
-println aecu
-        .contentUpgradeBuilder()
-        // collectors
-        .forDescendantResourcesOf("/content/we-retail/ca/en")
-        // filters
-        .filterByProperties(conditionMap_resourceType)
-        // actions
-        .printPath()
-        .doSetProperty("useFullWidth", false)
-        .run()
+aecu.contentUpgradeBuilder()
+    // collectors
+    .forDescendantResourcesOf("/content/we-retail/ca/en")
+    // filters
+    .filterByProperties(conditionMap_resourceType)
+    // actions
+    .printPath()
+    .doSetProperty("useFullWidth", false)
+    .run()
