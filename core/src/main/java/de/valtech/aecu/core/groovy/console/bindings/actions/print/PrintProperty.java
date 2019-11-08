a3289af64bdf268d6ca73dc658151f28ecda3bc9
sravan kumar sindhe <sravan.kumar@valtech.de>
2019-11-08 14:59:19 +0100

    fixed printing multi-value properties

--- core/src/main/java/de/valtech/aecu/core/groovy/console/bindings/actions/print/PrintProperty.java
+++ core/src/main/java/de/valtech/aecu/core/groovy/console/bindings/actions/print/PrintProperty.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Valtech GmbH
+ * Copyright 2019 Valtech GmbH
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -19,33 +19,65 @@
 
 package de.valtech.aecu.core.groovy.console.bindings.actions.print;
 
-import de.valtech.aecu.core.groovy.console.bindings.actions.Action;
-
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ValueMap;
+import java.util.Arrays;
 
 import javax.annotation.Nonnull;
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+import javax.jcr.Value;
+
+import org.apache.sling.api.resource.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.valtech.aecu.core.groovy.console.bindings.actions.Action;
 
 /**
  * Action for printing the name and value of a given property.
  *
+ * @author sravan
  * @author Roxana Muresan
  */
 public class PrintProperty implements Action {
 
-    private String propertyName;
+	private static final Logger LOG = LoggerFactory.getLogger(PrintProperty.class);
 
+	private final String NOT_DEFINED = " not defined";
+	private final String ERROR_MESSAGE = "Cannot read value of [{}]. Reason [{}]";
 
-    public PrintProperty(String propertyName) {
-        this.propertyName = propertyName;
-    }
+	private String propertyName;
 
-    @Override
-    public String doAction(@Nonnull Resource resource) {
-        ValueMap properties = resource.getValueMap();
-        if (properties.containsKey(propertyName)) {
-            return propertyName + " = " + properties.get(propertyName);
-        }
-        return propertyName + " not defined";
-    }
+	public PrintProperty(String propertyName) {
+		this.propertyName = propertyName;
+	}
+
+	@Override
+	public String doAction(@Nonnull Resource resource) {
+		Node node = resource.adaptTo(Node.class);
+		String propertyValue = propertyName + " = ";
+		try {
+			if (node.hasProperty(propertyName)) {
+				Property prop = node.getProperty(propertyName);
+
+				// This check is necessary to ensure a multi-valued property
+				if (prop.isMultiple()) {
+					Value[] values = prop.getValues();
+					String[] valuesAsStrings = new String[values.length];
+					for (int i = 0; i < values.length; i++) {
+						valuesAsStrings[i] = values[i].getString();
+					}
+					propertyValue = propertyValue + Arrays.toString(valuesAsStrings);
+				} else {
+					propertyValue = propertyValue + prop.getValue().getString();
+				}
+
+				return propertyValue;
+			}
+			return propertyName + NOT_DEFINED;
+		} catch (RepositoryException e) {
+			LOG.debug(ERROR_MESSAGE, propertyName, e.getMessage());
+		}
+		return "";
+	}
 }
