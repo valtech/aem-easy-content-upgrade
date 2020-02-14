9e94945b631ac0f21ea7e5ab0031df028ca38e57
Roxana Muresan <roxana.muresan@valtech.de>
2020-02-14 13:52:14 +0100

    feature/89_createNode: changed to dryRun() in example scripts and moved createNode script

--- examples/src/main/content/jcr_root/var/groovyconsole/scripts/aecu/aecu-examples/createNodes.groovy
+++ examples/src/main/content/jcr_root/var/groovyconsole/scripts/aecu/aecu-examples/createNodes.groovy
@@ -1,6 +1,5 @@
 def propertiesMap = [:]
 propertiesMap['test_with_props'] = "Test with properties"
-
 def propertiesMap2 = [:]
 propertiesMap2['test_with_props_and_relative'] = "Test with properties and relative path"
 propertiesMap2['another_property'] = "Another property value here"
@@ -11,4 +10,4 @@ println aecu.contentUpgradeBuilder()
         .doCreateResource("parent_props", "nt:unstructured", propertiesMap)
         .doCreateResource("relative_path", "nt:unstructured", "parent")
         .doCreateResource("relative_path_props", "nt:unstructured", propertiesMap2, "parent/relative_path")
-        .run()
+        .dryRun()
