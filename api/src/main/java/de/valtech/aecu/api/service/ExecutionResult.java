f43464a3fd9b904d2c30dbc5c813dc536e695a6b
Roland Gruber <roland.gruber@valtech.de>
2022-12-05 09:27:26 +0100

    fixed path shortening

--- api/src/main/java/de/valtech/aecu/api/service/ExecutionResult.java
+++ api/src/main/java/de/valtech/aecu/api/service/ExecutionResult.java
@@ -109,12 +109,16 @@ public class ExecutionResult {
     }
 
     /**
-     * Returns the short script path (stripped by prefix) or the unmodified path in case the script path does not start with {@link AecuService#AECU_VAR_PATH_PREFIX}.
+     * Returns the short script path (stripped by prefix) or the unmodified path in case the script
+     * path does not start with {@link AecuService#AECU_VAR_PATH_PREFIX}.
      *
      * @return short path
      */
     public String getShortPath() {
-        return path.replace(AecuService.AECU_VAR_PATH_PREFIX + "/", "");
+        String shortPath = path.replace(AecuService.AECU_VAR_PATH_PREFIX + "/", "");
+        shortPath = shortPath.replace(AecuService.AECU_CONF_PATH_PREFIX + "/", "");
+        shortPath = shortPath.replace(AecuService.AECU_APPS_PATH_PREFIX + "/", "");
+        return shortPath;
     }
 
     @Override
